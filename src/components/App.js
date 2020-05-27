e1a4af0aa511198bec83d7de8bcc63bc46b57f90
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 01:35:04 -0700

    display imported to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Display from './Display';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Display />
+        <Display result={0} />
         <ButtonPanel />
       </div>
     );
