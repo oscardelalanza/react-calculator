3ad1b0444fe213e06727f54b31d5e2961a4df637
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 08:36:59 -0700

    linter check  fixed

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,13 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      /* empty to avoid linter check */
+    };
+  }
+
   render() {
     return (
       <div className="container">
