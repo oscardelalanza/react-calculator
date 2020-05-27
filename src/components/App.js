1f85b0c135726670dcef8b3d905142a57a062338
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 03:44:07 -0700

    App class changed to pure funciton

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Display result={0} />
-        <ButtonPanel />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <Display result={0} />
+      <ButtonPanel />
+    </div>
+  );
 }
