57067aeba42b09b6eff50dbd38a48a1e0639ecac
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 02:45:30 -0700

    components rendered in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="container">
