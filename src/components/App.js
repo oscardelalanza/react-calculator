ea7442cebb62feac963e1a6d55346959ab3a7ac9
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 08:27:22 -0700

    App function changed to a class

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-export default function App() {
-  return (
-    <div className="container">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
