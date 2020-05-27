bb8f43e00368a9e59ad5a251113dd17b2d26cb36
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 02:40:17 -0700

    button pannel added

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,1 +1,44 @@
-// TODO: continue here point number 5 milestone 2
+import React from 'react';
+import Button from './Button';
+
+export default class ButtonPanel extends React.Component {
+  render() {
+    return (
+      <div className="button-panel">
+        <div className="group group-1">
+          <Button name="AC" />
+          <Button name="+\-" />
+          <Button name="%" />
+          <Button name="÷" />
+        </div>
+
+        <div className="group group-2">
+          <Button name="7" />
+          <Button name="8" />
+          <Button name="9" />
+          <Button name="X" />
+        </div>
+
+        <div className="group group-3">
+          <Button name="4" />
+          <Button name="5" />
+          <Button name="6" />
+          <Button name="-" />
+        </div>
+
+        <div className="group group-4">
+          <Button name="1" />
+          <Button name="2" />
+          <Button name="3" />
+          <Button name="+" />
+        </div>
+
+        <div className="group group-5">
+          <Button name="0" />
+          <Button name="." />
+          <Button name="=" />
+        </div>
+      </div>
+    );
+  }
+}
