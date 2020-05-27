0670591cc37eb6ad761822504d28d57b8477c774
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 01:41:09 -0700

    button component added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,18 @@
-// TODO: continue here point number 4 milestone 2
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Button extends React.Component {
+  render() {
+    return (
+      <button className="button">{this.props.name}</button>
+    );
+  }
+}
+
+Button.defaultProps = {
+  name: '',
+};
+
+Button.propTypes = {
+  name: PropTypes.string,
+};
