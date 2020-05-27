5b6d31bebc065f99564a6969acb829150d6fbcc8
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-27 03:39:25 -0700

    Display class changed to pure function

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Display extends React.Component {
-  render() {
-    return (
-      <div className="display">
-        {this.props.result}
-      </div>
-    );
-  }
+export default function Display({ result }) {
+  return (
+    <div className="display">
+      {result}
+    </div>
+  );
 }
 
 Display.defaultProps = {
