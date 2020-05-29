5dca25fbf47d910aa9db8585fa196728227765fd
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-29 13:07:54 -0700

    default button color changed

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,37 +5,37 @@ export default function ButtonPanel() {
   return (
     <div className="button-panel">
       <div className="group group-1">
-        <Button name="AC" />
-        <Button name="+\-" />
-        <Button name="%" />
-        <Button name="÷" color="btn-orange" />
+        <Button name="AC" color="btn-gray" />
+        <Button name="+\-" color="btn-gray" />
+        <Button name="%" color="btn-gray" />
+        <Button name="÷" />
       </div>
 
       <div className="group group-2">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="X" color="btn-orange" />
+        <Button name="7" color="btn-gray" />
+        <Button name="8" color="btn-gray" />
+        <Button name="9" color="btn-gray" />
+        <Button name="X" />
       </div>
 
       <div className="group group-3">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" color="btn-orange" />
+        <Button name="4" color="btn-gray" />
+        <Button name="5" color="btn-gray" />
+        <Button name="6" color="btn-gray" />
+        <Button name="-" />
       </div>
 
       <div className="group group-4">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" color="btn-orange" />
+        <Button name="1" color="btn-gray" />
+        <Button name="2" color="btn-gray" />
+        <Button name="3" color="btn-gray" />
+        <Button name="+" />
       </div>
 
       <div className="group group-5">
-        <Button name="0" width="btn-50" />
-        <Button name="." />
-        <Button name="=" color="btn-orange" />
+        <Button name="0" width="btn-50" color="btn-gray" />
+        <Button name="." color="btn-gray" />
+        <Button name="=" />
       </div>
     </div>
   );
