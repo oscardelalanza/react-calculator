5652adfbf0cc84007bc5f235096952dd99c4f879
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-05-29 12:21:37 -0700

    button styles added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({ name }) {
+export default function Button({ name, color, width }) {
+  let buttonClass = 'button';
+
+  if (color) {
+    buttonClass += ` ${color}`;
+  }
+
+  if (width) {
+    buttonClass += ` ${width}`;
+  }
+
   return (
-    <button type="button" className="button">{name}</button>
+    <button type="button" className={buttonClass}>{name}</button>
   );
 }
 
 Button.defaultProps = {
   name: '',
+  color: '',
+  width: '',
 };
 
 Button.propTypes = {
   name: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string,
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,34 +8,34 @@ export default function ButtonPanel() {
         <Button name="AC" />
         <Button name="+\-" />
         <Button name="%" />
-        <Button name="÷" />
+        <Button name="÷" color="btn-orange" />
       </div>
 
       <div className="group group-2">
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
-        <Button name="X" />
+        <Button name="X" color="btn-orange" />
       </div>
 
       <div className="group group-3">
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
-        <Button name="-" />
+        <Button name="-" color="btn-orange" />
       </div>
 
       <div className="group group-4">
         <Button name="1" />
         <Button name="2" />
         <Button name="3" />
-        <Button name="+" />
+        <Button name="+" color="btn-orange" />
       </div>
 
       <div className="group group-5">
-        <Button name="0" />
+        <Button name="0" width="btn-50" />
         <Button name="." />
-        <Button name="=" />
+        <Button name="=" color="btn-orange" />
       </div>
     </div>
   );
