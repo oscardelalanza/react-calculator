b7475b753b10c11adc6732aaf48b7d7e8f0c7afd
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-04 11:47:45 -0700

    calculate module imported to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
