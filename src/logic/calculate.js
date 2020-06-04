309afa139174dfe9b8481746f7be3294a90d4ade
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-04 11:30:19 -0700

    operate module applied in calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,16 +1,15 @@
+import operate from 'operate';
+
 export default function calculate(calculate = { total: null, next: null, operation: null }, btnName = '') {
-  let newCalculate = {
-    total: null,
-    next: null,
-    operation: null,
-  };
+  const operators = ['+', '-', 'X', '÷', '%'];
+  const newCalculate = calculate;
 
   if (btnName === '+/-') {
     newCalculate.total = calculate.total * -1;
     newCalculate.next = calculate.next * -1;
     newCalculate.operation = calculate.operation;
-  } else {
-    newCalculate = calculate;
+  } else if (operators.includes(btnName)) {
+    newCalculate.total = operate(calculate.total, calculate.next, btnName);
   }
 
   return newCalculate;
