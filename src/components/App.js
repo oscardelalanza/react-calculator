f337339d5aec53875325a6cadc4ffdba61f41d34
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-04 11:53:53 -0700

    no unused vars disabled for this milestone

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+// TODO: remove the following comment on the next milestone
+// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
