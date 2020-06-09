d990f46c05d2cd288054d271170de291a2130044
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-09 03:34:56 -0700

    handleclick defined

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// TODO: remove the following comment on the next milestone
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+    if (numbers.includes(buttonName)) {
+      // TODO: update state
+    }
+  }
+
   render() {
     return (
       <div className="app container">
