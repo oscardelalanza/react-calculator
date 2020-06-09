2f7f8e34c57dcb98767506c661cc293407d50a3f
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-09 16:01:22 -0700

    addDigit added with validation for just one dot

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ export default class App extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
     this.clearState = this.clearState.bind(this);
+    this.addDigit = this.addDigit.bind(this);
   }
 
   clearState() {
@@ -23,11 +24,32 @@ export default class App extends React.Component {
     });
   }
 
+  addDigit(buttonName) {
+    this.setState(state => {
+      let next = state.next || '';
+
+      if (buttonName === '.' && !state.next) {
+        next += `0${buttonName}`;
+      } else if (buttonName === '.') {
+        if (!next.includes('.')) {
+          next += buttonName;
+        }
+      } else {
+        next += buttonName;
+      }
+
+      return { next };
+    });
+  }
+
   handleClick(buttonName) {
-    const operators = ['+', '-', 'X', '÷', '%', '=', 'AC', '+/-'];
+    const operators = ['+', '-', 'X', '÷', '%', '=', '+/-'];
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 
     if (buttonName === 'AC') {
       this.clearState();
+    } else if (numbers.includes(buttonName)) {
+      this.addDigit(buttonName);
     }
   }
 
