7d77c3e3f5c7f93cef2a4a8cd92d56dbc5253244
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-09 04:11:18 -0700

    handleclick implemented in button panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,42 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default function ButtonPanel() {
-  return (
-    <div className="button-panel">
-      <div className="group group-1">
-        <Button name="AC" color="btn-gray" />
-        <Button name="+\-" color="btn-gray" />
-        <Button name="%" color="btn-gray" />
-        <Button name="÷" />
-      </div>
+export default class ButtonPanel extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-      <div className="group group-2">
-        <Button name="7" color="btn-gray" />
-        <Button name="8" color="btn-gray" />
-        <Button name="9" color="btn-gray" />
-        <Button name="X" />
-      </div>
+  handleClick(buttonName) {
+    const { clickHandler } = this.props;
+    clickHandler(buttonName);
+  }
 
-      <div className="group group-3">
-        <Button name="4" color="btn-gray" />
-        <Button name="5" color="btn-gray" />
-        <Button name="6" color="btn-gray" />
-        <Button name="-" />
-      </div>
+  render() {
+    return (
+      <div className="button-panel">
+        <div className="group group-1">
+          <Button name="AC" color="btn-gray" />
+          <Button name="+\-" color="btn-gray" />
+          <Button name="%" color="btn-gray" />
+          <Button name="÷" />
+        </div>
 
-      <div className="group group-4">
-        <Button name="1" color="btn-gray" />
-        <Button name="2" color="btn-gray" />
-        <Button name="3" color="btn-gray" />
-        <Button name="+" />
-      </div>
+        <div className="group group-2">
+          <Button name="7" color="btn-gray" />
+          <Button name="8" color="btn-gray" />
+          <Button name="9" color="btn-gray" />
+          <Button name="X" />
+        </div>
 
-      <div className="group group-5">
-        <Button name="0" color="btn-gray" wide />
-        <Button name="." color="btn-gray" />
-        <Button name="=" />
+        <div className="group group-3">
+          <Button name="4" color="btn-gray" />
+          <Button name="5" color="btn-gray" />
+          <Button name="6" color="btn-gray" />
+          <Button name="-" />
+        </div>
+
+        <div className="group group-4">
+          <Button name="1" color="btn-gray" />
+          <Button name="2" color="btn-gray" />
+          <Button name="3" color="btn-gray" />
+          <Button name="+" />
+        </div>
+
+        <div className="group group-5">
+          <Button name="0" color="btn-gray" wide />
+          <Button name="." color="btn-gray" />
+          <Button name="=" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
