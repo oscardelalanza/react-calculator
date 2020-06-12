e0cf5a5cc1887e39a4e7a50b908775a9421bb3df
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-06-12 01:46:07 -0700

    calculate first version

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,23 +1,18 @@
 import operate from './operate';
 
 export default function calculate(calculate = { total: null, next: null, operation: null }, btnName = '') {
-  const operators = ['+', '-', 'X', '÷', '%'];
   let total = parseFloat(calculate.total) || '';
   let next = parseFloat(calculate.next) || '';
-  const { operation } = calculate;
+  let { operation } = calculate;
 
   if (btnName === '+/-') {
     total = (total ? (total * -1).toString() : null);
     next = next ? (next * -1).toString() : null;
+  } else if (btnName === '=') {
+    total = operate(total, next, operation).toString();
+    next = null;
+    operation = null;
   }
 
-  // if (btnName === '+/-') {
-  //   newCalculate.total = calculate.total * -1;
-  //   newCalculate.next = calculate.next * -1;
-  //   newCalculate.operation = calculate.operation;
-  // } else if (operators.includes(btnName)) {
-  //   newCalculate.total = operate(calculate.total, calculate.next, btnName);
-  // }
-  //
   return { total, next, operation };
 }
